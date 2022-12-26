a5fb50b3f71482ea32f55cc1890d9a8feaef9761
trivenii3081 <trivenithikekar304@gmail.com>
2022-12-26 22:53:14 +0530

    initial commit

---
+++ file.java
@@ -0,0 +1,8 @@
+class student
+{
+public static void main()
+{
+int x =5;
+int y =6;
+}
+}
---
+++ file_change.java
@@ -0,0 +1,8 @@
+class student
+{
+public static void main()
+{
+int x =5;
+system.out.println("hello world: "+x);
+}
+}
