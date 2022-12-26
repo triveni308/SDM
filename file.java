7b5718b21cdf5eec1c700598a35a84cb4e884bdd
trivenii3081 <trivenithikekar304@gmail.com>
2022-12-26 23:11:36 +0530

    changed to file.java

--- file.java
+++ file.java
@@ -4,9 +4,8 @@ public static void main()
 {
 int x =5;
 int y =6;
-int z = 8;
-system.out.println("x: "+x);
-system.out.println("y: "+y);
-system.out.println("z: "+z);
+
+system.out.println("x: "+x+" y:"+y);
+
 }
 }
